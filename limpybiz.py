393aa833a3f5d6185a79ee794b3adca9a68c9d91
Nicolas Howe <ngarcia@desalesmedia.org>
2019-03-24 23:43:18 -0400

    add more phrases to repository

--- limpybiz.py
+++ limpybiz.py
@@ -4,14 +4,22 @@ import time
 import schedule
 
 last_rt = 0
-tweet = 'I hope you know I pack a chainsaw! https://www.youtube.com/watch?v=ZpUYjpKg9KY'
+tweets = ['I hope you know I pack a chainsaw! https://www.youtube.com/watch?v=ZpUYjpKg9KY',
+          'Some days you just don\'t wanna wake up https://www.youtube.com/watch?v=ZpUYjpKg9KY',
+          'The only fighting game I play is Fight Club https://www.youtube.com/watch?v=ZpUYjpKg9KY',
+          'I hope papa Fred notices me one day... https://www.youtube.com/watch?v=ZpUYjpKg9KY']
 # variables taken from config.py
 twitter = Twython(APP_KEY, APP_SECRET, OAUTH_TOKEN, OAUTH_TOKEN_SECRET)
 
 
+def get_random():
+    now = time.time()
+    return int(now % 4)
+
+
 def quality_content():
     print('right now I\'m dangerous')
-    twitter.update_status(status=tweet)
+    twitter.update_status(status=tweets[get_random()])
 
 
 def retweet_durst():
@@ -22,8 +30,8 @@ def retweet_durst():
         twitter.retweet(id=last_rt)
 
 
-schedule.every().day.at('16:24').do(quality_content())
-schedule.every().hour.do(retweet_durst())
+schedule.every().day.at('16:24').do(quality_content)
+schedule.every().hour.do(retweet_durst)
 
 
 try:
